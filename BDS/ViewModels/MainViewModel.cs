e4cfdae7c51c6026a2c9e6795d65fa20f337383c
Алексеев Алексей <Alale20002504@gmail.com>
2020-10-20 08:07:13 +0500

    Добавьте файлы проекта.

---
+++ BDS/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BDS.Annotations;
+
+namespace BDS.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        
+    }
+}
