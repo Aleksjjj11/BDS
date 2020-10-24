8764228b2611cf4eb19bc83f36f3e55621fb8c11
Pavel <war.naruto@mail.ru>
2020-10-24 15:31:32 +0500

    Created Evaluation class. Started dev. GetMedian()

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,18 +1,35 @@
 using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Net.Mime;
 
 namespace UnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void MedianOfArray_MustReturn_2()
         {
+            double[] testArr = { 1, 2, 3 };
+            double median = Evaluation.GetMedian(testArr);
+            Assert.AreEqual(2, median);
         }
 
         [Test]
-        public void Test1()
+        public void MedianOfArray_MustReturn_5()
         {
-            Assert.Pass();
+            double[] testArr = { 5, 5, 5, 10, 0 };
+            double median = Evaluation.GetMedian(testArr);
+            Assert.AreEqual(5, median);
+        }
+    }
+
+    public static class Evaluation
+    {
+        public static double GetMedian(double[] array)
+        {
+            return array.Sum(x => x) / array.Count();
         }
     }
 }
