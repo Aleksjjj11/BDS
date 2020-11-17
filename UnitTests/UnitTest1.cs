e6c40e1cf736cf83b8381b04244167b8aa0f6320
Pavel <war.naruto@mail.ru>
2020-11-17 10:43:46 +0500

    Added evaluation of sigma

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -32,5 +32,30 @@ namespace UnitTests
             double median = Evaluation.GetMedian(testArr);
             Assert.AreEqual(5, median);
         }
+
+        [Test]
+        public void SigmaOf_11_10_9_MustReturn_2()
+        {
+            double[] testArr = {11, 10, 9};
+            Evaluation.GetMedian(testArr);
+            double sigma = Evaluation.GetSigma(testArr);
+            Assert.AreEqual(2, sigma);
+        }
+    }
+
+    public static class Evaluation
+    {
+
+        private static double median;
+        private static double sigma;
+        public static double GetMedian(double[] array)
+        {
+            return median = array.Length == 0 ? 0 : array.Sum(x => x) / array.Count();
+        }
+
+        public static double GetSigma(double[] array)
+        {
+            return sigma = array.Length == 0 ? 0 : array.Sum(x => Math.Pow(x - median, 2));
+        }
     }
 }
